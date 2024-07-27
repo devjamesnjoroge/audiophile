8ea547fab2b7e3a3f3fc1ea20c668aa6680220bd
devjaymmy <developerjaymmy@gmail.com>
2024-07-27 22:49:17 +0300

    fx: speaker responsivenes

--- app/ui/hero.tsx
+++ app/ui/hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
         <>
         <section className="hero h-[70vh] md:h-[55vh] lg:h-[85vh] relative px-6 md:px-28 bg-zinc-900">
             <div className="absolute inset-0 bg-[url('/hero-m.png')] lg:bg-[url('/hero.png')] bg-contain bg-no-repeat bg-center"></div>
-            <div className="container absolute top-0 left-0 h-full gap-6 px-12 lg:px-28 text-white flex flex-col justify-center text-center lg:text-left items-center lg:items-start">
+            <div className="absolute top-0 left-0 h-full gap-6 px-12 lg:px-28 text-white flex flex-col justify-center text-center lg:text-left items-center lg:items-start">
             <h4 className="text-text-grey uppercase tracking-[.5em]">new product</h4>
             <h1 className="uppercase text-4xl lg:text-6xl font-bold tracking-widest lg:w-1/2">xx99 Mark II Headphones</h1>
             <p className="text-text-grey w-4/5 lg:w-2/5">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
